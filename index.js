5c731536575255ed84039d8759a1d1657fba1324
khanh196196 <mrkhanhqn007@gmail.com>
2024-10-20 20:46:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Just For Fun", "🎮 Playing VALORANT"];
+const statusMessages = ["Cái lồn có thể làm con cặc bạn sướng nhưng không thể đổi được tình anh em."];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
